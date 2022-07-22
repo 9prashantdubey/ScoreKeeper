7f302986d283ff6722b0660e37b5924093695e89
Prashant <9prashantdubey@gmail.com>
2022-07-22 14:30:10 +0530

    added winning score dropdown menu

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
-const reset = document.querySelector('#reset')
-const p1Display = document.querySelector('#p1Display')
-const p2Display = document.querySelector('#p2Display')
+const reset = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
+
 p1Button.addEventListener('click', function () {
     if (!isGameOver) {
         p1Score++;
         if (p1Score === winningScore) {
             isGameOver = true;
+            p1Display.classList.add('winner');
+            p2Display.classList.add('loser');
         }
         p1Display.textContent = p1Score;
     }
@@ -22,16 +26,28 @@ p1Button.addEventListener('click', function () {
 p2Button.addEventListener('click', function () {
     if (!isGameOver) {
         p2Score++;
-        if (p1Score === winningScore) {
+        if (p2Score === winningScore) {
             isGameOver = true;
+            p2Display.classList.add('winner');
+            p1Display.classList.add('loser');
         }
         p2Display.textContent = p2Score;
     }
 })
 
-reset.addEventListener('click', function () {
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    resetfun();
+})
+
+reset.addEventListener('click', resetfun);
+
+function resetfun() {
     p1Score = 0;
     p2Score = 0;
+    isGameOver = false;
     p1Display.textContent = p1Score;
     p2Display.textContent = p2Score;
-})
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+}
